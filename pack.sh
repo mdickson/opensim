6441d6d0defa1dbeb8758174a4814c35ab17baec
Mike Dickson <mike.dickson@utopiaskye.com>
2020-07-24 16:22:16 -0400

    Remove old addons prebuild config.  Update some gitignore settings

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 releasename=$1
 
-tar --exclude='./.git' --exclude='./.nant' --exclude='./.vs' --exclude='./obj' --exclude='./bin/ScriptEngines'  -czvf ../${releasename}.tar.gz .
+tar --exclude='./.git' --exclude='./.nant' --exclude='./.vs' --exclude='./obj' --exclude='./config' --exclude='./logs' --exclude='./bin/ScriptEngines'  -czvf ../${releasename}.tar.gz .
